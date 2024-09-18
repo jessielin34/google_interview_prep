c738c192ead866dce62b87e461d6789d7d2bae00
jessielin34 <80348079+jessielin34@users.noreply.github.com>
2024-09-17 21:28:27 -0400

    first commit

---
+++ two_sum.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+using namespace std;
+
+//return true or false if there are two numbers in an array that adds up to the target sum
+
+bool hasSum(const vector<int>& data, int sum) {
+    unordered_set<int> comp; //created a hashset
+    for (int value:data) { //for each value in the data vector
+        if (comp.find(value) != comp.end()) { //find if the value I'm currently on is in the hashset because I'm adding the complement of each number
+            return true; //if found, return true
+        }
+        comp.insert(sum - value); //otherwise, hashset add the value of the sum - value number
+    }
+    return false; //return false if no pairs add up to target sum
+}
